b972767f0f304dd1b1c9f4d1ae9112e477613de7
MichialM <michial.miller@gmail.com>
2023-01-14 12:56:47 -0500

    made adjustments to list objects

--- Book-Price-Trends--2022_v2/global.R
+++ Book-Price-Trends--2022_v2/global.R
@@ -11,5 +11,3 @@ library(shinydashboard)
 #Read in file and list objects
 prices <- readRDS("prices.RData")
 category_list <- unique(prices$BISAC_Level_1)
-pcb_list <- unique(prices$page_count_bucket)
-format_list <-  unique(prices$Format)

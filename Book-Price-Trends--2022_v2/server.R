26911e68a5330da7d4857ff675f229ec96b6c4f5
MichialM <michial.miller@gmail.com>
2023-01-14 12:10:18 -0500

    added plot codes and functional inputs

--- Book-Price-Trends--2022_v2/server.R
+++ Book-Price-Trends--2022_v2/server.R
@@ -9,23 +9,88 @@
 
 
 # Define server logic required to draw a histogram
-function(input, output, session) {
-
+function(input, output) { 
+  
   output$Category <- renderPrint({ input$Category })
   
   output$page_count_bucket <- renderPrint({ input$page_count_bucket })
   
-  output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-
-    })
-
-}
+  output$format <- renderPrint({ input$format })
+  
+  output$priceplot <- renderPlot({
+    p <- prices %>% 
+      filter(Format == input$format & 
+               BISAC_Level_1 == input$Category & 
+               page_count_bucket == input$page_count_bucket)
+    
+    median <- median(p$List_Price_Value, na.rm = TRUE) %>% 
+      format(round(2), nsmall = 2)
+    
+    price_plot <- p %>% 
+      ggplot(aes(x = List_Price_Value)) + 
+      geom_histogram(color = 1,  fill = 'skyblue', bins = input$bins) + 
+      geom_vline(xintercept = median(p$List_Price_Value, na.rm = TRUE), 
+                 linetype="dotted", 
+                 color = 'red',
+                 lwd = 1) +
+      coord_cartesian(expand = TRUE, clip = 'off') +
+      scale_x_continuous(breaks = seq(0,1000, by = 10))
+    
+    xlim <- layer_scales(price_plot)$x$range$range
+    ylim <- layer_scales(price_plot)$y$range$range
+    
+    price_plot + 
+      annotate("text",
+               x = xlim[2] /2 + 2,
+               y = ylim[2] /2 + 5,
+               label = paste("Median (---) = $", median),
+               color = "red",
+               size = 4) +
+      labs(x = 'List Price ($)', y = 'Frequency') +
+      labs(title = "2022 List Price Frequency Distribution") +
+      labs(caption = "Data: limited to 2022 pub dates w/ >= 3 reported sales (Ingram Book Company)")
+  })
+  
+  output$saturationplot <- renderPlot({
+    
+    p2 <- prices %>% 
+      filter(Format == input$format & BISAC_Level_1 == input$Category) %>%
+      group_by(BISAC_Level_1, BISAC_Level_2) %>% 
+      summarize(count = n()) %>%
+      arrange(desc(count))
+    
+    p3 <- p2[1:10, ] %>% 
+      mutate(relative_frequency = count/sum(count))
+    
+    p3 %>%   
+      ggplot(aes(x = reorder(BISAC_Level_2, -relative_frequency), y = relative_frequency)) +
+      geom_bar(stat = "identity", color = 1, fill = 'skyblue') +
+      labs(x = "Category", y = "Top 10 Frequency") +
+      labs(title = "Frequency of Top 10 Subcategories by Title Count") +
+      labs(caption = "Data: limited to 2022 pub dates w/ >= 3 reported sales (Ingram Book Company)") +
+      labs(legend.position = 'none') +
+      guides(x = guide_axis(angle = -45))
+  })
+  
+  output$opportunityplot <- renderPlot({
+    
+    p4 <- prices %>% 
+      filter(Format == input$format & BISAC_Level_1 == input$Category) %>%
+      group_by(BISAC_Level_2) %>% 
+      transmute(sales_per_title = Sales_2022/n_distinct(ISBN)) %>%
+      arrange(desc(sales_per_title)) %>% 
+      filter(duplicated(BISAC_Level_2) == FALSE)
+    
+    p5 <- p4[1:20, ]
+    
+    p5 %>% 
+      ggplot(aes(x = sales_per_title, 
+                 y = reorder(BISAC_Level_2, sales_per_title))) +
+      geom_col(color = 1, fill = 'skyblue') +
+      labs(x = "Sales per Title", y = "Category") +
+      labs(title = "Top 20 Subcategories -- Sales per Title") +
+      labs(caption = "Data: limited to 2022 pub dates w/ >= 3 reported sales (Ingram Book Company)") +
+      theme(legend.position='none')
+  })
+  
+  }
